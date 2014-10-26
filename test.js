244b5527f5e3abb027fcdde062678ae1286ab28d
Mu-An Chiou <me@muanchiou.com>
2014-10-27 02:39:48 +0800

    does it work?

--- test.js
+++ test.js
@@ -2,6 +2,5 @@ var fs = require('fs')
 var tomd = require('./index.js')
 
 fs.readFile('./test.md', function(err, data) {
-  var doo = tomd(data.toString())
-  console.log(doo)
+  console.log(tomd(data.toString()))
 })
